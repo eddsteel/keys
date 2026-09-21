ff5c3db501fce18459b103ea6bcd4199883ccc8b
Edd Steel <edd@eddsteel.com>
2026-09-21 16:05:57 -0700

    match latest moonlander layout

--- keyboards/crkbd/rev4_1/standard/keymaps/eddsteel/config.h
+++ keyboards/crkbd/rev4_1/standard/keymaps/eddsteel/config.h
@@ -25,6 +25,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
 
+#define FLOW_TAP_TERM 100
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
+
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
--- keyboards/crkbd/rev4_1/standard/keymaps/eddsteel/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/eddsteel/keymap.c
@@ -17,19 +17,68 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 // TODO
 // - RGB matrix
-// - japanese mode
+// - lighting switch on language change
 
 #include QMK_KEYBOARD_H
 
-#define KE_MEGV MEH_T(KC_GRV)
+// pinky mods
+#define KE_LAGV LALT_T(KC_GRV)
+#define KE_LAQT LALT_T(KC_QUOT)
 #define KE_LGLB LGUI_T(KC_LBRC)
-#define KE_LATA LALT_T(KC_TAB)
-#define KE_LSEN LSFT_T(KC_ENT)
 #define KE_RGRB RGUI_T(KC_RBRC)
-#define KE_HYQU ALL_T(KC_QUOT)
+
+// base home row
+#define KE_LCKS LCTL_T(KC_S)
+#define KE_LAKD LALT_T(KC_D)
+#define KE_LGKF LGUI_T(KC_F)
+#define KE_MEKG MEH_T(KC_G)
+#define KE_HYKH ALL_T(KC_H)
+#define KE_RGKJ RGUI_T(KC_J)
+#define KE_LAKK LALT_T(KC_K)
+#define KE_RCKL RCTL_T(KC_L)
+
+// nav/num home row
+#define KE_LCK7 LCTL_T(KC_7)
+#define KE_LAK8 LALT_T(KC_8)
+#define KE_LGK9 LGUI_T(KC_9)
+#define KE_RGDN RGUI_T(KC_UP)
+#define KE_LAUP LALT_T(KC_DOWN)
+#define KE_RCRT RCTL_T(KC_RGHT)
+
+// function home row
+#define KE_LCMT LCTL_T(KC_MUTE)
 #define KE_LADL LALT_T(KC_DEL)
-#define KE_L2SP LT(2, KC_SPC)
+#define KE_LGPS LGUI_T(KC_PSCR)
+#define KE_RGF6 RGUI_T(KC_F6)
+#define KE_LAF7 LALT_T(KC_F7)
+#define KE_RCF8 RCTL_T(KC_F8)
+
+// symbol home row
+#define KE_LCBS LCTL_T(KC_BSLS)
+#define KE_LAPD LALT_T(KC_DOT)
+#define KE_LGSL LGUI_T(KC_SLSH)
+// symbols are out of range
+
+// thumbs
+#define KE_LAY3 OSL(3)
+#define KE_L1SP LT(1, KC_SPC)
+#define KE_L2TA LT(2, KC_TAB)
+#define KE_L2EN LT(2, KC_ENT)
 #define KE_L1BS LT(1, KC_BSPC)
+#define KE_SHFT OSM(MOD_LSFT)
+
+// keys with mods
+#define KE_STAB S(KC_TAB)
+#define KE_LGEN G(KC_ENT)
+#define KE_NDSH LSA(KC_MINS)
+#define KE_MDSH A(KC_MINS)
+#define KE_ELPS A(KC_SCLN)
+#define KE_DEGR LSA(KC_8)
+#define KE_BULL A(KC_8)
+#define KE_FLSQ A(KC_RBRC)
+#define KE_FLDQ A(KC_LBRC)
+#define KE_FRDQ LSA(KC_LBRC)
+#define KE_FRSQ LSA(KC_RBRC)
 #define KE_LCGQ LCG(KC_Q)
 
 enum custom_keycodes {
@@ -39,8 +88,9 @@ enum custom_keycodes {
 
 enum {
   BASE = 0,
-  NMBA,
-  MDIA,
+  NNUM,
+  FUNN,
+  SYMB,
   KYBD,
   NLYRS
 };
@@ -63,27 +113,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x6_3_ex2(
-        KE_MEGV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_BSLS,     KC_EQL,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KE_HYQU,
-        SC_LCPO,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_ESC,      KC_MINS,   KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, SC_RCPC,
+        KE_LAGV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_BSLS,     KC_EQL,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KE_LAQT,
+        SC_LCPO,  KC_A, KE_LCKS, KE_LAKD, KE_LGKF, KE_MEKG,   KC_ESC,    KC_MINS, KE_HYKH, KE_RGKJ, KE_LAKK, KE_RCKL, KC_SCLN, SC_RCPC,
         KE_LGLB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KE_RGRB,
-                                          KE_LATA,  KE_L2SP,  KE_LSEN,   KC_RSFT, KE_L1BS, KE_LADL
+                                          KE_LAY3, KE_L1SP,  KE_L2TA,    KE_L2EN, KE_L1BS, KE_SHFT
     ),
-    [NMBA] = LAYOUT_split_3x6_3_ex2(
-       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_SLEP,    QK_BOOT, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_LCBR, KC_RCBR,
-       _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_COLN,    _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, _______,
-       _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_DOT,                      KC_PLUS,  KC_EQL,   KC_LT,   KC_GT, KC_LBRC, _______,
-                                           _______,   TG(3), _______,    _______, _______,  KC_APP
+    [NNUM] = LAYOUT_split_3x6_3_ex2(
+       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_SLEP,    _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_WBAK, QK_BOOT,
+       _______,    KC_6, KE_LCK7, KE_LAK8, KE_LGK9,    KC_0, _______,    _______, KC_LEFT, KE_RGDN, KE_LAUP, KE_RCRT, KC_WFWD, _______,
+       _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_DOT,                      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_PLUS, _______,
+                                           KC_BSPC, _______, KE_STAB,    _______,  KC_DEL, _______
     ),
-    [MDIA] = LAYOUT_split_3x6_3_ex2(
-       _______, KC_MPLY, KC_MRWD, KC_MFFD, KC_MUTE, KC_VOLU, QK_BOOT,    KE_LCGQ, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_BRIU,  KC_F15,
-       _______,  KC_INS, KC_PAUS,  KC_DEL, KC_PSCR, KC_VOLD,   KC_F6,      KC_F7, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BRID,  KC_F14,
-       _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_F13,
-                                           _______, _______, _______,    _______,  OSL(3),  KC_APP
+    [FUNN] = LAYOUT_split_3x6_3_ex2(
+       QK_BOOT, KC_MFFD, KC_VOLU,  KC_INS, KC_BRIU, _______, _______,    KE_LCGQ,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F15, QK_BOOT,
+       _______, KC_MPLY, KE_LCMT, KE_LADL, KE_LGPS, _______, _______,    _______,   KC_F5, KE_RGF6, KE_LAF7, KE_RCF8,  KC_F14, _______,
+       _______, KC_MRWD, KC_VOLD, KC_PAUS, KC_BRID, _______,                        KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_F13, _______,
+                                           _______,   TG(4), _______,    KE_LGEN,   TG(4), _______
+    ),
+    [SYMB] = LAYOUT_split_3x6_3_ex2(
+       KC_TILD, KC_MINS, KE_NDSH, KE_MDSH, KC_UNDS,  KC_GRV, KE_ELPS,    KC_QUES, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_LCBR, KC_RCBR,
+       KE_DEGR, KC_PIPE, KE_LCBS, KE_LAPD, KE_LGSL, KC_QUOT, KC_COLN,    KC_SCLN, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, _______,
+       KE_BULL, KE_FLSQ, KE_FLDQ, KE_FRDQ, KE_FRSQ, KC_DQUO,                      KC_PLUS,  KC_EQL,   KC_LT,   KC_GT, KC_LBRC, _______,
+                                           _______, _______, _______,    KC_ENT,  KC_SPC,   KC_APP
     ),
     [KYBD] = LAYOUT_split_3x6_3_ex2(
-         TO(0), XXXXXXX, XXXXXXX, KE_MAC1, KE_MAC2, XXXXXXX, RGB_M_P,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+       QK_BOOT, XXXXXXX, XXXXXXX, KE_MAC1, KE_MAC2, XXXXXXX, RGB_M_P,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
        _______, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU, XXXXXXX, RM_TOGG,    XXXXXXX, RGB_M_R, RGB_M_B,RGB_M_SW, XXXXXXX, XXXXXXX, XXXXXXX,
        _______, RM_HUED, RM_SATD, RM_VALD, RM_SPDD, RM_NEXT,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                           _______, _______, _______,    _______, _______, _______
+                                             _______, TO(0), _______,    _______,   TO(0), _______
     )
 };
